a02bcca21a52de8cc344089a2d11aafaca998a4b
joão Victor <joaoeusoueu13225@gmail.com>
2023-05-09 18:57:00 -0300

    so sei que nada sei

---
+++ A4.py
@@ -0,0 +1,21 @@
+x = 1
+x = 1.5
+x = "Eu"
+x = True #boll
+n1 = 7;n2 = 3;x = complex(1j)
+
+#print(x.real)
+#print(x.imag)
+
+x = ["carros", "motos", "barco"] #lista/Array
+x[0] = "lancha"
+x = ("carros", "motos", "barco") #tupla
+x = range(0,100,1) #gerador de listas
+x = {
+    "rede":"Globo",
+    "canal":"10"
+} #dicionario
+
+print("Valor: " ,x)
+print("Valor: " ,x["rede"])
+print("Tipo: ", type(x))
