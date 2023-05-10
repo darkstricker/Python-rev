ad196de9ed52d7fd46264054f13b9700f0842ba0
joão Victor <joaoeusoueu13225@gmail.com>
2023-05-10 18:06:15 -0300

    mudancinha

---
+++ A5.py
@@ -0,0 +1,15 @@
+import random
+
+num_i = 38
+num_c = 1j
+num_f = 3.12
+num_R = random.randrange(0, 99)
+
+x = float(num_i)
+y = int(num_f)
+z = num_c
+
+print("Valor: ", x, " - Tipo: ", type(x))
+print("Valor: ", y, " - Tipo: ", type(y))
+print("Valor: ", z, " - Tipo: ", type(z))
+print("Valor: ", num_R, " - Tipo: ", type(num_R))
