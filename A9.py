da16c293dd52b3262e45de760949cc4e0a1edf2c
joão Victor <joaoeusoueu13225@gmail.com>
2023-05-29 16:57:40 -0300

    listas

---
+++ A9.py
@@ -0,0 +1,13 @@
+carros = ["UNO", "Fiat", "Argo"]
+carros.append("Golf")
+print(len(carros))
+
+print(carros)
+carros.pop()
+print(carros)
+
+carros2 = ["fusca", "celta", "147"]
+
+carros3 = carros + carros2
+
+print(carros3)
