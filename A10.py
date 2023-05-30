852426d14e086f3c719bef3ba8100ff96edddc57
joão Victor <joaoeusoueu13225@gmail.com>
2023-05-30 17:47:11 -0300

    if

---
+++ A10.py
@@ -0,0 +1,15 @@
+a = True
+
+if a:
+    print('Sim ele é')
+
+print("fim do programa")
+
+if 10 > 5:
+    print("O maioral é 10")
+
+if 4 == 9:
+    print("Você é maluco é?")
+
+else:
+    print("Acertó")
