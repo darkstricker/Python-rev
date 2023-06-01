f620bde858c7eed11f2eeea1d862aae2ef0806e5
joão Victor <joaoeusoueu13225@gmail.com>
2023-06-01 16:52:25 -0300

    elif

--- A10.py
+++ A10.py
@@ -8,7 +8,7 @@ print("fim do programa")
 if 10 > 5:
     print("O maioral é 10")
 
-if 4 == 9:
+if 4 != 9:
     print("Você é maluco é?")
 
 else:
---
+++ A11.py
@@ -0,0 +1,26 @@
+clima = input("digite o clima (calor)/(frio): ")
+dinheiro = 500
+
+if clima == "calor":
+    print("vai comprar um açai")
+
+    print("vou gastar R$27")
+
+elif clima == "frio":
+    print("tu vai ficar com frieira")
+
+    print("vou gastar R$0")
+
+'''AND
+
+# V + V = V
+# V + F = F
+# F + V = F
+# F + F = F'''
+
+'''OR
+
+# V + V = V
+# V + F = V
+# F + V = V
+# F + F = F'''
