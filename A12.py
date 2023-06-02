c03e035d792937b3255fd265e5e676e93a8082ee
joão Victor <joaoeusoueu13225@gmail.com>
2023-06-02 18:17:25 -0300

    for + input

---
+++ A12.py
@@ -0,0 +1,9 @@
+frutas = ["maca", "banana", "uva passa","pera"]
+
+for x in frutas:
+    print(x)
+
+    if x == "uva passa":
+        print("   Gosto não")
+
+print("fim do programa")
---
+++ A13.py
@@ -0,0 +1,14 @@
+import os
+
+os.system('clear')
+
+nome = input("Digite seu nome: ")
+
+print(nome)
+
+num1 = int(input("digite o primeiro valor: "))
+num2 = int(input("digite o segundo valor: "))
+
+r = num1 + num2
+
+print("o valor é", r)
